f6b430b301e255b3c936bc8a631bd66cd411a28c
Adrian Louie De Guzman <adrianlouie27@gmail.com>
2019-01-17 22:02:13 +0800

    Initial changes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function Main() {
 
     function onRequest(request, response) {
         response.writeHead(200, {"Content-Type": "text/html"});
-        response.write("<html><head><title>Home</title></head><body>Hello, World</body></html>")
+        response.write("<html><head><title>Home</title></head><body><p>Hello, World</p><p>Hello, World</p><p>Hello, World</p><p>Hello, World</p><p>Hello, World</p></body></html>")
         response.end();        
     }
 
