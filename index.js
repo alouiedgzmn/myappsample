91fae19e169ab422113a58370fb1c1ade2ff0107
Adrian Louie De Guzman <adrianlouie27@gmail.com>
2019-01-18 19:44:42 +0800

    Initial Changes

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 function Main() {
     const http = require('http');
-    var port = 443;
+    var port = 8080;
 
     function onRequest(request, response) {
         response.writeHead(200, {"Content-Type": "text/html"});
-        response.write("<html><head><title>Home</title></head><body><p>Hello, World</p><p>Hello, World</p><p>Hello, World</p><p>Hello, World</p><p>Hello, World</p></body></html>")
-        response.end();        
-    }
+        response.write("<html><head><title>Hello, World!</title></head><body><p>Hello, World!</p><p>Hello, World!</p><p>Hello, World!</p></body></html>")
+        response.end();      
+  }
 
     http.createServer(onRequest).listen(port);
 
-    console.log("Listening on port 443...");
-}   
-
+    console.log("Listening on port 8080...");
+}
 Main();
